a6fedb4e861a870a7cc44340c7feaeb7c9506626
Brandon <pacewicbrandon@gmail.com>
2022-07-01 12:04:16 -0700

    Added round b solutions

---
+++ contests/google_kick_start/2022/roundB/A.cpp
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+template <typename A, typename B>
+std::ostream& operator<<(std::ostream& os, const std::pair<A, B>& p) {
+    return os << '(' << p.first << ", " << p.second << ')';
+}
+
+template <typename T_container,
+          typename T = typename std::enable_if<
+              !std::is_same<T_container, std::string>::value,
+              typename T_container::value_type>::type>
+std::ostream& operator<<(std::ostream& os, const T_container& container) {
+    os << '{';
+    std::string separator;
+
+    for (const T& item : container) {
+        os << separator << item, separator = ", ";
+    }
+
+    return os << '}';
+}
+
+#ifdef DBG_MODE
+void dbg_out() { std::cerr << std::endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head A, Tail... B) {
+    std::cerr << ' ' << A;
+    dbg_out(B...);
+}
+#define test(...) std::cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define test(...)
+#endif
+
+void run_case(const uint16_t& tc) {
+    int R, A, B;
+    cin >> R >> A >> B;
+
+    vector<double> areas;
+    bool step = true;
+    int previous_radius = R;
+
+    auto to_area = [](int radius) -> double { return M_PI * radius * radius; };
+
+    while (previous_radius > 0) {
+        areas.push_back(to_area(previous_radius));
+
+        if (step) {
+            previous_radius *= A;
+        } else {
+            previous_radius /= B;
+        }
+
+        step = !step;
+    }
+
+    long double ans = accumulate(areas.begin(), areas.end(), 0.0);
+    cout << fixed << setprecision(6);
+    cout << "Case #" << tc << ": " << ans << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    uint16_t test_cases;
+    std::cin >> test_cases;
+
+    for (uint16_t tc = 1; tc <= test_cases; tc++) {
+        run_case(tc);
+#ifdef DBG_MODE
+        std::cout << std::flush;
+#endif
+    }
+
+#ifndef DBG_MODE
+    std::cout << std::flush;
+#endif
+
+    return 0;
+}
